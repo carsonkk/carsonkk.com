173b1f9e3b1b182ec71b39deb3392a7f07711f94
carsonkk <carsonkk@gmail.com>
2019-05-17 21:47:50 -0700

    Updates

--- public/README.md
+++
@@ -1,1 +0,0 @@
-This is the production branch of [carsonkk.com](http://carsonkk.com). For the original source code, check the master branch.
--- src/utils/Responsive.js
+++
@@ -1,5 +0,0 @@
-export const MinWidth = {
-  s: "40em",  // 640px  @ 16px root
-  m: "52em",  // 832px  @ 16px root
-  l: "64em"   // 1024px @ 16px root
-}
